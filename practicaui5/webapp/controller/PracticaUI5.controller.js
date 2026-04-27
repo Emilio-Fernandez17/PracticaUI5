0103f78cf2f04b712a6346224ac889702b409cbf
Emilio-Fernández17 <emilimo.wispf.11@gmail.com>
2026-04-27 16:48:52 +0200

    commit idiomas 2

--- practicaui5/webapp/controller/PracticaUI5.controller.js
+++ practicaui5/webapp/controller/PracticaUI5.controller.js
@@ -1,10 +1,42 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Configuration",
+    "sap/ui/model/resource/ResourceModel"
 ], (Controller) => {
     "use strict";
 
     return Controller.extend("practicaui5.practicaui5.controller.PracticaUI5", {
         onInit() {
+            this._sCurrentLanguage = "es";
+        },
+
+         onButtonPress: function () {
+            // Alternamos entre "es" y "en"
+
+            var sNewLang = this._sCurrentLanguage === "es" ? "en" : "es";
+
+            this._sCurrentLanguage = sNewLang;
+
+
+            // Creamos un nuevo ResourceModel con el idioma elegido
+
+            var oNewModel = new ResourceModel({
+
+                bundleName: "practicaui5.practicaui5.i18n.i18n",
+
+                supportedLocales: ["", "en", "es"],
+
+                fallbackLocale: "",
+
+                bundleLocale: sNewLang
+
+            });
+
+
+            // Reemplazamos el modelo i18n en la vista
+
+            this.getView().setModel(oNewModel, "i18n");
+
         }
     });
 });
