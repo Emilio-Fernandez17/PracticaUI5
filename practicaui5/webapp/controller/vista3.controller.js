9a73728f93666accd3245062ee527effd9f117dc
Emilio-Fernández17 <emilimo.wispf.11@gmail.com>
2026-05-05 13:33:59 +0200

    commit arreglos

--- practicaui5/webapp/controller/vista3.controller.js
+++ practicaui5/webapp/controller/vista3.controller.js
@@ -9,6 +9,7 @@ sap.ui.define([
 
         onInit() {
             this.cargarTabla();
+            this._sCurrentLanguage = "es";
         },
 
         async cargarTabla() {
@@ -26,6 +27,14 @@ sap.ui.define([
                 MessageBox.error("Error al cargar datos: " + err.message);
             }
         },
+
+        volverAPrincipal: function() {
+            const oRouter = this.getOwnerComponent().getRouter();
+            if (oRouter) {
+                oRouter.navTo("RoutePracticaUI5");
+            }
+        },
+
         async GuardarDatos() {
             const oModel = this.getView().getModel("permisosModel");
             const aDatos = oModel.getData();
@@ -52,8 +61,25 @@ sap.ui.define([
                 sap.ui.core.BusyIndicator.hide();
                 this.getOwnerComponent().getRouter().navTo("Routevista2");
             }
-        }
+        },
 
+        onButtonPress: function () {
+            var sNewLang = this._sCurrentLanguage === "es" ? "en" : "es";
+            this._sCurrentLanguage = sNewLang;
+
+            var oNewModel = new ResourceModel({
+                bundleName: "practicaui5.practicaui5.i18n.i18n",
+                supportedLocales: ["", "en", "es"],
+                fallbackLocale: "",
+                bundleLocale: sNewLang
+            });
+
+
+            this.getView().setModel(oNewModel, "i18n");
+
+
+            this.getOwnerComponent().setModel(oNewModel, "i18n");
+        }
 
     });
 });
